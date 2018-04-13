fad968c224a5a4a1367d84ea1f23c862bf0e71b4
Adrian Garza <adrianhgarza@gmail.com>
2018-04-12 22:39:46 -0400

    merge error whoops

--- code/main.py
+++ code/main.py
@@ -8,8 +8,6 @@ tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 from flask import Flask, make_response, request, url_for, redirect, render_template
 import MySQLdb
 
-from google.appengine.api import users
-
 
 # These environment variables are configured in app.yaml.
 CLOUDSQL_CONNECTION_NAME = os.environ['CLOUDSQL_CONNECTION_NAME']
@@ -17,7 +15,6 @@ CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
 CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
 
 app = Flask(__name__, template_folder=tmpl_dir)
-
 
 def connect_to_cloudsql():
     # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
@@ -73,93 +70,91 @@ def showDatabases():
     return response
 
 
-@app.route('/index.html', methods=['POST'])
+@app.route('/index.html', methods=['POST', 'GET'])
 def create_user():
-    f_name = request.form['first_name_field']
-    l_name = request.form['last_name_field']
-    uni = request.form['uni_field']
-    password = request.form['password_field']
-    school = request.form['school_field']
-    year = request.form['year_field']
-    interests = request.form['interests_field']
+    error = None
 
-    # connect to db
-    db = connect_to_cloudsql()
-    cursor = db.cursor()
-    cursor.execute('use cuLunch')
-
-    # check if uni is already registered
-    uniqueuni_query = "SELECT uni from users"
-    cursor.execute(uniqueuni_query)
-    registered_uni = set([row[0] for row in cursor.fetchall()])
-    unique = uni not in registered_uni
-
-    form_input = Form(f_name, l_name, uni, password, school, year, interests)
-    user_check, error = form_input.form_input_valid()
-
-    print (form_input.uni + " " + form_input.f_name + " " + form_input.l_name + " " + form_input.school +
-           " " + form_input.interests + " " + form_input.school + " " + form_input.pwd)
-
-    if user_check and unique:
-
-        name = form_input.f_name + ' ' + form_input.l_name
-        user = User(uni, name, year, interests, school, password)
-        # else send error to user
-
-        # store in database
+    if request.method == 'POST':
+        f_name = request.form['first_name_field']
+        l_name = request.form['last_name_field']
+        uni = request.form['uni_field']
+        password = request.form['password_field']
+        school = request.form['school_field']
+        year = request.form['year_field']
+        interests = request.form['interests_field']
 
 
+        # connect to db
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        cursor.execute('use cuLunch')
 
-        insert_query = "INSERT INTO users VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (user.uni, user.password, user.name,
-                                                                                   user.schoolYear, user.interests, user.school)
-        # print('query generated')
-        # print(query)
-
-        try:
-            cursor.execute(insert_query)
-            # commit the changes in the DB
-            db.commit()
-        except:
-            # rollback when an error occurs
-            db.rollback()
-
-        # disconnect from db after use
-        db.close()
-        return redirect(url_for('create_listing'))
-
-    elif not unique:
-        error = 'This UNI has been registered already.'
-        db.close()
+        # check if uni is already registered
+        uniqueuni_query = "SELECT uni from users"
+        cursor.execute(uniqueuni_query)
+        registered_uni = set([row[0] for row in cursor.fetchall()])
+        unique = uni not in registered_uni
 
 
-    else:
-        #return redirect(url_for('static', filename='index.html', error=error))
-        db.close()
+        form_input = Form(f_name, l_name, uni, password, school, year, interests)
+        user_check, error = form_input.form_input_valid()
+
+        print (form_input.uni + " " + form_input.f_name + " " + form_input.l_name + " " + form_input.school +
+               " " + form_input.interests + " " + form_input.school + " " + form_input.pwd)
+
+        '''if not user_check:
+            error = error
+            db.close()'''
+
+
+        if user_check and unique:
+
+            name = form_input.f_name + ' ' + form_input.l_name
+            user = User(uni, name, year, interests, school, password)
+            # else send error to user
+
+            # store in database
+
+
+
+            insert_query = "INSERT INTO users VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (user.uni, user.password, user.name,
+                                                                                       user.schoolYear, user.interests, user.school)
+            # print('query generated')
+            # print(query)
+
+            try:
+                cursor.execute(insert_query)
+                # commit the changes in the DB
+                db.commit()
+            except:
+                # rollback when an error occurs
+                db.rollback()
+
+            # disconnect from db after use
+            db.close()
+            return redirect(url_for('create_listing'))
+
+        elif user_check == (False, 'bad pass'):
+            error = 'Password is not valid: length of password is at least 8, and it should contain at all three of the \
+                    following: digits, uppercase letters, and lowercase letters.'
+            db.close()
+            return render_template('index.html', error=error)
+
+        elif user_check == (False, 'empty'):
+            error = 'Empty answer in one field'
+            db.close()
+            return render_template('index.html', error=error)
+
+        elif not unique:
+            error = 'This UNI has been registered already.'
+            db.close()
+
+
+        else:
+            # return redirect(url_for('static', filename='index.html', error=error))
+            db.close()
+
     return render_template('index.html', error=error)
-
-
-@app.route('/index.html', methods=['GET'])
-def render_index():
-    # first check if the user is signed in
-    # TODO: boot them if they're signed in with a non-columbia id (insecure since it's client-side but whatever)
-    # then if the user is signed in, check if they already have an account
-    # if they don't, render the creation form with their first/last name info already filled in
-    # if they do, take them to the listing page
-    user = users.get_current_user()
-    if user:
-        nickname = user.nickname()
-        logout_url = users.create_logout_url('/index.html')
-        greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-            nickname, logout_url)
-    else:
-        login_url = users.create_login_url('/index.html')
-        greeting = '<a href="{}">Sign in</a>'.format(login_url)
-
-    # then render the index page depending on whether the user has been signed in
-    # we need the index page template to have a field to change for this
-    # if no user: give the register page
-    # if user: render the listings page
-    return greeting
 
 
 @app.route('/listform/index.html', methods=['POST', 'GET'])
@@ -170,7 +165,7 @@ def create_listing():
         timestamp = request.form['timestamp']
         needSwipe = request.form.get('needswipe') != None
         # print(cafeteria, timestamp, needSwipe)
-        
+
 
         # store in database
         db = connect_to_cloudsql()
@@ -191,11 +186,11 @@ def create_listing():
 
         # disconnect from db after use
         db.close()
-      
+
         return redirect(url_for('output'))
 
 
-    return render_template('/listform/index.html')
+    return render_template('/listings/index.html')
 
 
 @app.route('/listings')
