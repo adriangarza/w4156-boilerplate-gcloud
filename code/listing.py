79f2e804d06a6175e157a8a305ba849fda5a78bc
Shelley  Simon <shelleysimon@dyn-160-39-140-222.dyn.columbia.edu>
2018-04-13 19:43:50 -0400

    Fixing the import error

--- code/listing.py
+++ code/listing.py
@@ -1,4 +1,3 @@
-# from dateutil.parser import parse
 import datetime
 
 class Listing:
@@ -9,7 +8,7 @@ class Listing:
         self.uni = uni
         self.place = place
 
-'''class ListForm:
+class ListForm:
 
 	def __init__ (self, cafeteria, date, time, swipe):
 		#type:(object, object, object, boolean)
@@ -20,10 +19,17 @@ class Listing:
 
 	def day_of_week(self):
 		#parsing the dateime input
-		b= parse(self.date)
-		wkday = b.weekday()
+		date = self.date
+		y, m, d = [int(i) for i in date.split('-')]
+		wkday = datetime.date(y, m, d).weekday()
 		return wkday
 		#0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
+
+	def time_parser(self):
+		t = self.time
+		h, m = [int(i) for i in t.split(':')]
+		time = datetime.time(h, m, 0)
+		return time
 	
 	def time_range (start, end, x):
 		if start <= end:
@@ -38,6 +44,7 @@ class Listing:
 			lChecker = False
 			error = "Need to pick a cafeteria"
 		else:
+			time = time_parser()
 			day = day_of_week()
 			#Ferris Booth Hours
 			if self.cafeteria == "Ferris Booth":
@@ -46,15 +53,15 @@ class Listing:
 				elif day == 5: #Saturday hours
 					start = datetime.time (9, 0, 0)
 					end = datetime.time(21, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 				elif day == 4: #Friday hours
 					start = datetime.time (7, 30, 0)
 					end = datetime.time(21, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 				else:
 					start = datetime.time (7, 30, 0)
 					end = datetime.time(20, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 
 			# John Jay Hours
 			elif self.cafeteria == "John Jay":
@@ -63,40 +70,40 @@ class Listing:
 				else:
 					start = datetime.time (9, 30, 0)
 					end = datetime.time(21, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 
 			#JJ's Hours
 			elif self.cafeteria == "JJs Place":
 				start = datetime.time (12, 0, 0)
 				end = datetime.time(10, 0, 0)
-				lChecker = time_range(start, end, self.time)
+				lChecker = time_range(start, end, time)
 
 			#Hewitt Hours
 			elif self.cafeteria == "Hewitt":
 				if day == 6: #Sunday hours
 					start = datetime.time (10, 0, 0)
 					end = datetime.time(15, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 					if lChecker == False:
 						start = datetime.time (17, 0, 0)
 						end = datetime.time(19, 45, 0)
-						lChecker = time_range(start, end, self.time)
+						lChecker = time_range(start, end, time)
 				elif day == 5: #Saturday hours
 					start = datetime.time (10, 0, 0)
 					end = datetime.time(15, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 					if lChecker == False:
 						start = datetime.time (17, 0, 0)
 						end = datetime.time(19, 0, 0)
-						lChecker = time_range(start, end, self.time)
+						lChecker = time_range(start, end, time)
 				else:
 					start = datetime.time (8, 0, 0)
 					end = datetime.time(15, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 					if lChecker == False:
 						start = datetime.time (16, 0, 0)
 						end = datetime.time(19, 45, 0)
-						lChecker = time_range(start, end, self.time)
+						lChecker = time_range(start, end, time)
 			#Diana Hours
 			elif self.cafeteria == "Diana":
 				if day == 5: #Saturday closed
@@ -104,35 +111,35 @@ class Listing:
 				elif day == 6: #Sunday hours
 					start = datetime.time (17, 0, 0)
 					end = datetime.time(19, 45, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 					if lChecker == False:
 						start = datetime.time (20, 45, 0)
 						end = datetime.time(23, 0, 0)
-						lChecker = time_range(start, end, self.time)
+						lChecker = time_range(start, end, time)
 				elif day == 4: #Friday hours
 					start = datetime.time (9, 30, 0)
 					end = datetime.time(11, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 					if lChecker == False:
 						start = datetime.time (11, 30, 0)
 						end = datetime.time(15, 0, 0)
-						lChecker = time_range(start, end, self.time)
+						lChecker = time_range(start, end, time)
 				else:
 					start = datetime.time (9, 30, 0)
 					end = datetime.time(11, 0, 0)
-					lChecker = time_range(start, end, self.time)
+					lChecker = time_range(start, end, time)
 					if lChecker == False:
 						start = datetime.time (11, 30, 0)
 						end = datetime.time(15, 0, 0)
-						lChecker = time_range(start, end, self.time)
+						lChecker = time_range(start, end, time)
 						if lChecker == False:
 							start = datetime.time (17, 0, 0)
 							end = datetime.time(19, 45, 0)
-							lChecker = time_range(start, end, self.time)
+							lChecker = time_range(start, end, time)
 							if lChecker == False:
 								start = datetime.time (20, 45, 0)
 								end = datetime.time(23, 0, 0)
-								lChecker = time_range(start, end, self.time)
+								lChecker = time_range(start, end, time)
 			if lChecker == False:
 				error = self.cafeteria + "is not open at the time selected"
 		return lChecker, error'''
