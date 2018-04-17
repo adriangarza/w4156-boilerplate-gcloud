0f47fb6035ff5979292482dcb3ba0466ea925c8d
shelleysmn <shelleyannsimon_2017@depauw.edu>
2018-04-16 21:35:19 -0400

    removed a password related error

--- code/main.py
+++ code/main.py
@@ -130,11 +130,6 @@ def create_user():
             error = 'Empty answer in one field'
             db.close()
 
-        elif not user_check and error == 'bad pass':
-            error = 'Password is not valid: length of password is at least 8, and it should contain at all three of the \
-                    following: digits, uppercase letters, and lowercase letters.'
-            db.close()
-
         elif not unique:
             error = 'This UNI has been registered already.'
             db.close()
