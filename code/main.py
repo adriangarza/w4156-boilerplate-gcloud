b771d2dcbba7dd191afc8d2747d022c472b5b7d9
carsonkraft <carsonk200@gmail.com>
2018-04-23 19:31:26 -0400

    Better search functional

--- code/main.py
+++ code/main.py
@@ -379,8 +379,10 @@ def search_listings():
         db.close()
 
         # serve index template
-        return render_template('/listings/index.html', locnumlistings = loc_num_listings, locswipes= loc_swipes, place=cafeteria, current_user=me, listingposts=posts, name=user.nickname (),
-                            logout_link=users.create_logout_url("/"))
+        return render_template('/listings/index.html', locnumlistings=loc_num_listings,
+                               locswipes=loc_swipes, place=cafeteria, current_user=me,
+                               listingposts=posts, name=user.nickname(),logout_link=users.create_logout_url("/"), needs=show_swipe_needers,
+                               offers=show_swipe_offerers)
 
 
 def check_registered_user(uni):
