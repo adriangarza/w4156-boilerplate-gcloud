8f76e8016c5aaba0443fb3d602bceafe67bee18c
shelleysmn <shelleyannsimon_2017@depauw.edu>
2018-04-24 16:20:26 -0400

    Fixing this error

--- code/main.py
+++ code/main.py
@@ -258,7 +258,7 @@ def output():
 
     # grab the relevant information and make sure the user doesn't see their own listings there
     query = "SELECT u.uni, u.name, u.schoolYear, u.interests, u.schoolName, l.expiryTime, l.needsSwipes, l.Place from " \
-            "users u JOIN listings l ON u.uni=l.uni WHERE NOT u.uni = '{}' AND l.expiryTime > NOW() ORDER BY l.expiryTime".format(uni)
+            "users u JOIN listings l ON u.uni=l.uni WHERE NOT u.uni = '{}' ORDER BY l.expiryTime".format(uni)
 
     me = find_user(uni)
     try:
@@ -274,7 +274,8 @@ def output():
         u = User(r[0], r[1], r[2], r[3], r[4])
         # we need to convert datetime into a separate date and time for the listing object
         l = Listing(r[5], r[0], r[7], r[6])
-        posts.append(ListingPost(l, u))
+        if l.expiryDateTime > (datetime.datetime.now() - timedelta(hours=4)):
+            posts.append(ListingPost(l, u))
         print(str(l.expiryDateTime) + " ")
         num_listings +=1
         if l.needSwipe:
@@ -475,7 +476,7 @@ def show_profile():
     for r in cursor.fetchall():
         u = User(r[3], r[4], r[5], r[6], schools[r[7]])
         l = Listing(r[0], uni, r[2], r[1])
-        if l.expiryDateTime > (datetime.datetime.now()-- timedelta(hours=4)):
+        if l.expiryDateTime > (datetime.datetime.now()- timedelta(hours=4)):
             listingposts.append(ListingPost(l, u))
     db.close()
 
